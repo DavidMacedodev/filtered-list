bb06700c909df654b1d53939becc5517ff4355e2
David Macedo <newlima159@gmail.com>
2024-06-19 16:40:27 -0300

    feat: add pt-BR logic in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DATE_PIPE_DEFAULT_OPTIONS, DatePipeConfig } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StatusPipe } from './pipes/status.pipe';
+import { UserStatusImagePipe } from './pipes/user-status-image.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
+import { FormsModule } from '@angular/forms';
+
+const datePipeConfig: DatePipeConfig = {
+  dateFormat: 'dd/MM/YYYY',
+  timezone: '+0000'
+}
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatusPipe
+    StatusPipe,
+    UserStatusImagePipe,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: datePipeConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
